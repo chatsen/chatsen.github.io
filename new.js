9c021615f246c7ede66e4712f940b624f97a1cb8
williambulin <williambulin01@gmail.com>
2021-12-09 18:04:49 +0100

    change: removing redundent code

--- new.js
+++ new.js
@@ -1,26 +1,24 @@
 let themeSwitcherLight = document.getElementById('theme-switcher-light');
 let themeSwitcherDark = document.getElementById('theme-switcher-dark');
 
-themeSwitcherLight.addEventListener('click', function () {
-  document.documentElement.classList.replace('dark', 'light');
-  themeSwitcherLight.style.display = 'none';
-  themeSwitcherDark.style.display = 'block';
-  localStorage.setItem('theme', 'light');
-});
+if (localStorage.getItem('theme') === 'dark')
+  applyDarkTheme();
+else if (localStorage.getItem('theme') === 'light')
+  applyLightTheme();
 
-themeSwitcherDark.addEventListener('click', function () {
+function applyDarkTheme() {
   document.documentElement.classList.replace('light', 'dark');
   themeSwitcherLight.style.display = 'block';
   themeSwitcherDark.style.display = 'none';
   localStorage.setItem('theme', 'dark');
-});
+}
 
-if (localStorage.getItem('theme') === 'dark') {
-  document.documentElement.classList.replace('light', 'dark');
-  themeSwitcherLight.style.display = 'block';
-  themeSwitcherDark.style.display = 'none';
-} else if (localStorage.getItem('theme') === 'light') {
+function applyLightTheme() {
   document.documentElement.classList.replace('dark', 'light');
   themeSwitcherLight.style.display = 'none';
   themeSwitcherDark.style.display = 'block';
+  localStorage.setItem('theme', 'light');
 }
+
+themeSwitcherLight.addEventListener('click', applyLightTheme);
+themeSwitcherDark.addEventListener('click', applyDarkTheme);
