a8ca001f7a885efab598e7be901e79357ac94a61
williambulin <williambulin01@gmail.com>
2021-12-09 18:01:19 +0100

    add: theme save

--- new.js
+++ new.js
@@ -5,10 +5,22 @@ themeSwitcherLight.addEventListener('click', function () {
   document.documentElement.classList.replace('dark', 'light');
   themeSwitcherLight.style.display = 'none';
   themeSwitcherDark.style.display = 'block';
+  localStorage.setItem('theme', 'light');
 });
 
 themeSwitcherDark.addEventListener('click', function () {
   document.documentElement.classList.replace('light', 'dark');
   themeSwitcherLight.style.display = 'block';
   themeSwitcherDark.style.display = 'none';
+  localStorage.setItem('theme', 'dark');
 });
+
+if (localStorage.getItem('theme') === 'dark') {
+  document.documentElement.classList.replace('light', 'dark');
+  themeSwitcherLight.style.display = 'block';
+  themeSwitcherDark.style.display = 'none';
+} else if (localStorage.getItem('theme') === 'light') {
+  document.documentElement.classList.replace('dark', 'light');
+  themeSwitcherLight.style.display = 'none';
+  themeSwitcherDark.style.display = 'block';
+}
